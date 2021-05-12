0cb26da54ffde4895b6b3b1601c296acb70a970c
simba-fs <simba.fs@gmail.com>
2021-05-12 14:52:22 +0800

    next.config.js

--- deploy.js
+++ deploy.js
@@ -3,8 +3,8 @@ var ghpages = require('gh-pages');
 ghpages.publish('out', {
 	dotfiles: true,
 	// add: true,
-	branch: 'page',
-	repo: 'simba@simba-fs.dev:website/fakebook',
-	message: 'Deploy to github page',
+	// branch: 'page',
+	// repo: 'simba@simba-fs.dev:website/fakebook',
+	// message: 'Deploy to github page',
 
 }, console.error);
