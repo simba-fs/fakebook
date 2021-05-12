679f1ac6b442d5a0ff513732e654b5080f3b02ec
simba-fs <simba.fs@gmail.com>
2021-05-12 10:18:35 +0800

    test

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,10 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
 	return (
 		<div className={styles.grid}>
-			<Link href="/user">
+			<Link href="./user">
 				<a className={styles.card}>User</a>
 			</Link>
-			<Link href="/post">
+			<Link href="./post">
 				<a className={styles.card}>Post</a>
 			</Link>
 		</div>
